1b117ad76398c6367e84c251dbd56c38a7e6a003
asalem <ariel.salem1989@gmail.com>
2023-11-16 10:08:09 -0800

    add pokedex divider and loader

---
+++ src/Divider.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export const Divider = () => (
+  <div className="divider">
+    <div className="gap" />
+    <div className="hinge" />
+    <div className="gap" />
+    <div className="hinge" />
+    <div className="gap" />
+    <div className="hinge" />
+    <div className="gap" />
+  </div>
+);
---
+++ src/Loader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.css';
+
+export const Loader = () => (
+  <div className="loading-container">
+    <img
+      src="https://imgs.search.brave.com/Xo3OHxxm4jw4pvwC0PX88fzmQzZi6Tdd9yqJazgfLxY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvNC9Qb2tl/YmFsbC1QTkcucG5n"
+      className="loading-pokeball"
+      alt="Pokeball loader"
+    />
+  </div>
+);
