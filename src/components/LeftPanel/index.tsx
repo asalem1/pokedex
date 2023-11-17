b55dbf252237ec9db0ffb1cdae5ca9a5b1779b8f
asalem <ariel.salem1989@gmail.com>
2023-11-17 06:17:02 -0800

    format Divider.css and add the left panel

---
+++ src/components/LeftPanel/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './LeftPanel.css';
+import {Loader} from '../Loader';
+import {PokemonImage} from '../PokemonImage';
+
+interface Props {
+  pData: any;
+  id: number;
+  description: string;
+}
+
+export function LeftPanel(props: Props) {
+  const pData = props.pData;
+  const description = props.description ?? 'Loading....'
+  const id = props.id ?? 'XXXX';
+  const name = pData?.name ?? 'Loading...'
+  const hasSprites = pData.sprites != null;
+  return (
+    <div className="panel left-panel">
+      <div className="pokemon-name screen">
+        {name}
+        <span className="name-no">no. {id}</span>
+      </div>
+      {hasSprites ? <PokemonImage src={pData.sprites} /> : <Loader />}
+      <div className="pokemon-description screen">{description}</div>;
+    </div>
+  );
+}
